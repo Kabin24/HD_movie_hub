38645a08583ae27c4b3900ef36189ab1d34c2177
kabin shrestha <108089715+Kabin24@users.noreply.github.com>
2024-01-15 23:13:08 +0545

    added

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,29 @@
 import { useState ,useEffect} from 'react'
-
+import { BrowserRouter,Routes,Route } from 'react-router-dom'
 import './App.css'
 import { fetchDataFromApi } from './utils/api'
 import { useSelector,useDispatch } from 'react-redux'
 import { getApiConfiguration } from './store/homeslice'
 
 
+
+import Header from "./components/header/Header";
+import Footer from "./components/footer/Footer";
+import Home from "./pages/home/Home";
+import Details from "./pages/details/Details";
+import SearchResult from "./pages/searchResult/SearchResult";
+import Explore from "./pages/explore/Explore";
+import PageNotFound from './pages/404/pageNotFound'
+
+
 function App() {
   const  dispatch = useDispatch()
-   const {url } = useSelector
+  
+   const {url} = useSelector((state) =>
+   state.home
+   )
+   console.log(url)
+
     useEffect (() => {
     apiTesting();
   },[])
@@ -21,9 +36,20 @@ function App() {
         })
        }
   
-  return 
-      <div className='App'> App</div>
-  
+  return (
+    <BrowserRouter>
+    <Header/>
+    <Routes>
+    
+     <Route path='/' element={<Home/>} />
+     <Route path="/:mediaType/:id" element={<Details />} />
+     <Route path="/search/:query" element={<SearchResult />} />
+     <Route path="/explore/:mediaType" element={<Explore />} />
+     <Route path="*" element={<PageNotFound />} />
+    </Routes>
+    <Footer/>
+    </BrowserRouter>
+  )
 }
 
 export default App
