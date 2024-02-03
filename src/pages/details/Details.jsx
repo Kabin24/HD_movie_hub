8eb9d7c4165e9f434ce74c237fb15e77f7121956
kabin shrestha <108089715+Kabin24@users.noreply.github.com>
2024-02-03 22:48:01 +0545

    added top cast & official video

--- src/pages/details/Details.jsx
+++ src/pages/details/Details.jsx
@@ -3,13 +3,20 @@ import { useParams } from 'react-router-dom'
 import "./style.scss"
 import DetailsBanner from './detailsBanner/DetailsBanner'
 import useFetch from '../../hooks/useFetch'
+import Cast from './cast/Cast'
+import VideosSection from './videosSection/VideosSection'
+import { LoadingMessage } from 'react-select/dist/declarations/src/components/Menu'
 const Details = () => {
   const {mediaType,id} = useParams();
   const {data,loading} = useFetch(`/${mediaType}/${id}/videos`)
-   const {data: credits,loading} = useFetch(`/${mediaType}/${id}/videos`)
+  const {data:credits , loading: creditsLoading} = useFetch(`/${mediaType}/${id}/credits`)
+
+  
   return (
     <div>
-      <DetailsBanner/>
+      <DetailsBanner video={data?.results[0]} crew={credits?.crew}/>
+      <Cast  data={credits?.cast} loading={creditsLoading}/>
+      <VideosSection data={data} loading={loading}/>
     </div>
   )
 }
